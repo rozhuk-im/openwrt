67904d5d03be53a5e7fb9bec4dae517e339a9537
Rozhuk Ivan <rozhuk.im@gmail.com>
2020-06-13 03:00:03 +0300

    FreeBSD build fix
    Signed-off-by: Rozhuk Ivan <rozhuk.im@gmail.com>

---
+++ tools/cmake/files/InitialCache.cmake
@@ -0,0 +1,15 @@
+# Disable CMake's tests while building.
+set(BUILD_TESTING OFF CACHE BOOL "Build the testing tree.")
+
+# Force CMake to look for base's liblzma, otherwise the configuration process
+# will fail if archivers/lzmalib is installed, as CMake will try to use its
+# liblzma.so.
+# Note that this is necessary only for `make configure' to work, as liblzma is
+# only ever used if CMake's bundled libarchive is being used.
+set(LIBLZMA_INCLUDE_DIR "/usr/include" CACHE PATH
+    "Directory where LibLZMA headers are located.")
+set(LIBLZMA_LIBRARY "/usr/lib/liblzma.so" CACHE PATH
+    "LibLZMA library to link against.")
+
+# Disable CPACK.
+set(CPACK_ENABLE_FREEBSD_PKG OFF CACHE BOOL "Enable pkg(8) generator in CPack")
